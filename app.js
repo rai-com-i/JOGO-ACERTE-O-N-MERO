36a7fb2e8c2f4f9a152e1c2b18bfad86afd271b4
rai-com-i <151753763+rai-com-i@users.noreply.github.com>
2024-06-29 10:57:39 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNatela(tag, texto){
+     let campo = document.querySelector(tag);
+     campo.innerHTML = texto
+}
+
+function exibirMensagemInicial(){
+    exibirTextoNatela('h1', 'Jogo do número secreto');
+    exibirTextoNatela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto){
+        exibirTextoNatela('h1', 'Acertou!');
+        let palavraTentativas = tentativas > 1 ? 'Tentativas':'Tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativas}`;
+        exibirTextoNatela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    } else{
+        if (chute > numeroSecreto){
+            exibirTextoNatela('p', 'O número secreto é menor!');
+        } else{
+            exibirTextoNatela('p', 'O número secreto é maior!');
+        }
+        tentativas++;
+        limparCampo();
+    }
+    
+
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1)
+}
+
+function limparCampo(){
+    chute = document.querySelector('input').value = '';
+}
+
+function reiniciarJogo(){
+    console.log ('funcionando')
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
